c63ee7fef28c8f13dfb8896caf14a89c6edd3611
Foxrim <Flavien085@gmail.com>
2025-03-24 15:25:41 +0100

    [feat] - Header terminé, media screen tablette

--- src/header/components/social networks/Social.component.tsx
+++ src/header/components/social networks/Social.component.tsx
@@ -5,46 +5,28 @@ import linkedin from "../../../assets/Linkedin.png";
 import mail from "../../../assets/Mail.png";
 import { NavLink } from "react-router";
 
-import data from "../../../data/data.json"
-import { useEffect, useState } from "react";
+import data from "../../../data/data.json";
 
 export default function Social() {
-  const [isMobile, setIsMobile] = useState<boolean>();
-
-  useEffect(() => {
-    if (window.innerWidth < 880) {
-      setIsMobile(true);
-    } else {
-      setIsMobile(false);
-    }
-  }, []);
-  
-
   return (
     <nav className={styles.social}>
       <a href={data.contacts[0].link}>
         <figure>
           <img src={github} alt="Logo github" />
         </figure>
-        {!isMobile && (
-          <p>{data.contacts[0].name}</p>
-        )}
+        <p>{data.contacts[0].name}</p>
       </a>
       <a href={data.contacts[1].link}>
         <figure>
           <img src={linkedin} alt="Logo Linkedin" />
         </figure>
-        {!isMobile && (
-          <p>{data.contacts[1].name}</p>
-        )}
+        <p>{data.contacts[1].name}</p>
       </a>
       <NavLink to="/contacts">
-          <figure>
-            <img src={mail} alt="Logo mail" />
-          </figure>
-          {!isMobile && (
-          <p>Mail</p>
-        )}
+        <figure>
+          <img src={mail} alt="Logo mail" />
+        </figure>
+        <p>Mail</p>
       </NavLink>
     </nav>
   );
