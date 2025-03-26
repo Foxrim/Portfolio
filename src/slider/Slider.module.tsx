86d94f6e1573de0031ad747f96981850d486a7c6
Foxrim <Flavien085@gmail.com>
2025-03-26 17:52:09 +0100

    [feat] - slider fonctionnel mobile

--- src/slider/Slider.module.tsx
+++ src/slider/Slider.module.tsx
@@ -2,23 +2,48 @@ import styles from "./Slider.module.css";
 
 import Card from "./components/card/Card.component";
 import data from "../data/data.json";
+import useSlider from "./hooks/useSlider";
 
 export default function Slider() {
   const projects = data.projects;
+  const projectShow = 3;
+
+  const {
+    visibleProjects,
+    sliderRef,
+    projectIndex,
+    handleTouchStart,
+    handleTouchMove,
+    handleTouchEnd,
+    handleCurrentIndex
+  } = useSlider(projects, projectShow);
 
   return (
     <section className={styles.sliderContainer}>
-      {projects.map((project) => (
-        <Card
-          title={project.title}
-          image={project.image}
-          description={project.description}
-          github={project.github || null}
-          website={project.website || null}
-        />
-      ))}
+      <div
+        className={styles.slider}
+        ref={sliderRef}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+      >
+        {visibleProjects.length > 0 &&
+          visibleProjects.map((project, index) => (
+            <Card
+              key={index}
+              title={project.title}
+              image={project.image}
+              description={project.description}
+              github={project.github || null}
+              website={project.website || null}
+            />
+          ))}
+      </div>
+
       <div className={styles.buttonContainer}>
-        <button />
+        {visibleProjects.map((project, index) => (
+          <button type="button" key={`index${index}-${project.title}`} className={index === projectIndex ? styles.active : ""} onClick={() => handleCurrentIndex(index)}/>
+        ))}
       </div>
     </section>
   );
