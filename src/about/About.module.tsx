1b4fe8bfb07c9c6640e1b07519dc8dddc0b1deb7
Foxrim <Flavien085@gmail.com>
2025-03-29 16:04:28 +0100

    [feat] - About version desktop

--- src/about/About.module.tsx
+++ src/about/About.module.tsx
@@ -2,6 +2,7 @@ import Presentation from "./components/presentation/Presentation.component";
 import Technologie from "./components/technologie/Technologie.component";
 import data from "../data/data.json";
 import styles from "./About.module.css";
+import Profile from "./components/profile/Profile.component";
 
 export default function About() {
   const technologies = data.technologies.types;
@@ -9,13 +10,16 @@ export default function About() {
   return (
     <section className={styles.about}>
       <Presentation />
-      {Object.entries(technologies).map(([categoryName, technologyList]) => (
-        <Technologie
-          key={categoryName}
-          categoryName={categoryName}
-          technologies={technologyList}
-        />
-      ))}
+      <div className={styles.techContainer}>
+        {Object.entries(technologies).map(([categoryName, technologyList]) => (
+          <Technologie
+            key={categoryName}
+            categoryName={categoryName}
+            technologies={technologyList}
+          />
+        ))}
+      </div>
+      <Profile />
       <span className={styles.circle} />
       <footer>©flavien-rousseau</footer>
     </section>
